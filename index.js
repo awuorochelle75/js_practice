8d7e4cecd2d4ae2a434092de89112da1d3d4911d
Rochelle Awuor <awuorochelle75@gmail.com>
2026-03-17 08:56:57 +0300

    Adds content using the element id in js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log("Hello")
 
-window.alert(`This is  Rochelle learning Js!`)
+//window.alert(`This is  Rochelle learning Js!`)
+
+document.getElementById("my-heading").textContent=`Hello Rochelle`;
+document.getElementById("my-para").textContent="Welcome to Gemini - your most desired AI platform"
