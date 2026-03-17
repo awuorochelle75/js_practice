f31e7efaa45642e0c8354b8857a5bc1488980b20
Rochelle Awuor <awuorochelle75@gmail.com>
2026-03-17 09:25:47 +0300

    Adds the different types of variables

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ console.log("Hello")
 
 //window.alert(`This is  Rochelle learning Js!`)
 
-document.getElementById("my-heading").textContent=`Hello Rochelle`;
-document.getElementById("my-para").textContent="Welcome to Gemini - your most desired AI platform"
+//document.getElementById("my-heading").textContent=`Hello Rochelle`;
+//document.getElementById("my-para").textContent="Welcome to Gemini - your most desired AI platform"
+
+//VARIABLES
+let x;
+x = 123;
+
+console.log(x);
+
+//Data types
+let age=25;
+console.log(age);
