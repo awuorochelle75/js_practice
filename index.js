06bd64a96eade42bcbe90aac1596f6074cfcc3cb
Rochelle Awuor <awuorochelle75@gmail.com>
2026-03-19 09:11:19 +0300

    Adds math.pow to raise a base to a given power

--- index.js
+++ index.js
@@ -168,14 +168,15 @@ resetBtn.onclick = function () {
 
 //Math - built in objects that provides a collection of properties and methods
 
-let x = 3.99;
+let x = 3;
 let y = 2;
 let z;
 
 //z = Math.round(x);
 //z = Math.floor(x);
 //z = Math.ceil(x);
-z = Math.trunc(x)
+//z = Math.trunc(x);
+z = Math.pow(x, y);
 
 
 console.log(z);
